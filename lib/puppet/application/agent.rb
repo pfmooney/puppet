1172f5329b3ca8151593d75054b1aeb577bd70eb
Patrick Mooney <patrick.f.mooney@gmail.com>
2012-11-06 14:19:58 -0600

    Add parens for ruby 1.8 support

--- lib/puppet/application/agent.rb
+++ lib/puppet/application/agent.rb
@@ -403,7 +403,7 @@ Copyright (c) 2011 Puppet Labs, LLC Licensed under the Apache 2.0 License
     # if --no-client is set.
     require 'puppet/agent'
     require 'puppet/configurer'
-    @agent = Puppet::Agent.new(Puppet::Configurer, not(Puppet[:onetime]))
+    @agent = Puppet::Agent.new(Puppet::Configurer, (not(Puppet[:onetime])))
 
     enable_disable_client(@agent) if options[:enable] or options[:disable]
 
